4f4b8351578ab9c44075f2a3c6707474477aa68d
TeodorMiller <georgejostulov@gmail.com>
2019-10-26 02:46:52 +0300

    make some changes

--- tanos.cpp
+++ tanos.cpp
@@ -13,8 +13,10 @@ int main(){
    do {
       sorted = true;
       for (int i = 0; i < c.size() - 2 ; ++i )
-           if (c.at(i) > c.at(i + 1))
+           if (c.at(i) > c.at(i + 1)){
                   sorted = false;
+                  break;
+           }
       if (!sorted)
            for (int i = 0 ; i < (c.size() - 1) / 2 ; ++i )
                    c.erase(c.begin());
